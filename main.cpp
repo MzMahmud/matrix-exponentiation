56ae5aaec023883dfdf32609a91d4d6821b9874f
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2021-09-03 11:53:27 +0600

    build error fixed

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
     cout << "hello world!" << endl;
-    Matrix<int> a;
+    Matrix<int> a(3, 2, -1);
+    cout << a;
     return 0;
 }
