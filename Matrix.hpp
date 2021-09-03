dbb60510232f4eb614cc8fd98e70f3ea4dcb32e9
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2021-09-03 10:56:24 +0600

    test code added for matrix class

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,26 +1,28 @@
 #ifndef __MATRIX_HPP__
 #define __MATRIX_HPP__
 
+#include <cstddef>
+
 template <class T> class Matrix {
   public:
-    Matrix();
-    Matrix(const Matrix &other);
-    Matrix(size_t n_row, size_t n_col);
-    Matrix(size_t n_row, size_t n_col, const T &initial_value);
-    ~Matrix();
+    Matrix() {}
+    Matrix(const Matrix &other) {}
+    Matrix(size_t n_row, size_t n_col) {}
+    Matrix(size_t n_row, size_t n_col, const T &initial_value) {}
+    ~Matrix() {}
 
-    Matrix &operator=(const Matrix &other);
-    Matrix &operator+=(const Matrix &other);
-    Matrix &operator-=(const Matrix &other);
-    Matrix &operator*=(const Matrix &other);
+    Matrix &operator=(const Matrix &other) {}
+    Matrix &operator+=(const Matrix &other) {}
+    Matrix &operator-=(const Matrix &other) {}
+    Matrix &operator*=(const Matrix &other) {}
 
-    Matrix operator+(const Matrix &other);
-    Matrix operator-(const Matrix &other);
-    Matrix operator*(const Matrix &other);
+    Matrix operator+(const Matrix &other) {}
+    Matrix operator-(const Matrix &other) {}
+    Matrix operator*(const Matrix &other) {}
 
-    bool operator==(const Matrix &other);
+    bool operator==(const Matrix &other) {}
 
-    T &operator()(size_t i, size_t j);
+    T &operator()(size_t i, size_t j) {}
 
   private:
     T *a;
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
+#include "Matrix.hpp"
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
     cout << "hello world!" << endl;
+    Matrix<int> a;
     return 0;
 }
