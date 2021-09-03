2ccf412741520eb610ac527a2585884991bfe1c1
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2021-09-03 15:15:11 +0600

    matrix multiplication implemented
    assignment operator bug fixed

--- main.cpp
+++ main.cpp
@@ -3,26 +3,35 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-    cout << "hello world!" << endl;
-    Matrix<int> a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    cout << "[a]\n";
-    cout << a;
-    {
-        Matrix<int> b = a;
-        b(0, 0)       = 10;
-        cout << "[b]\n";
-        cout << b;
+Matrix<int> getI(int n) {
+    Matrix<int> I(n, n, 0);
+    for (int i = 0; i < n; ++i)
+        I(i, i) = 1;
+    return I;
+}
 
-        cout << "[a+b]\n";
-        cout << (a + b);
-        cout << "[a-b]\n";
-        cout << (a - b);
-        cout << "[a]\n";
-        cout << a;
-        cout << "[b]\n";
-        cout << b;
-    }
+Matrix<int> matrix_exponentiation(const Matrix<int> &a, int n) {
+    if (n == 0)
+        return getI(a.get_n_row());
+    Matrix<int> x = matrix_exponentiation(a, n >> 1);
+    x             = x * x;
+    return (n & 1) ? x * a : x;
+}
+
+int main(int argc, char const *argv[]) {
+    Matrix<int> a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    // Matrix<int> b = {{1, 2}, {3, 4}, {5, 6}};
+
+    cout << "[a]";
+    cout << a;
+    // cout << "[b]\n";
+    // cout << b;
+
+    // cout << "[ab]\n";
+    // cout << a * b;
+
+    cout << "[a^10]\n";
+    cout << matrix_exponentiation(a, 5);
 
     return 0;
 }
