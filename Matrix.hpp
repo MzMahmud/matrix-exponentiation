d85f92b1df5450242c36959cadb10140157b9130
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2021-09-03 10:49:04 +0600

    matrix class structure added

---
+++ Matrix.hpp
@@ -0,0 +1,30 @@
+#ifndef __MATRIX_HPP__
+#define __MATRIX_HPP__
+
+template <class T> class Matrix {
+  public:
+    Matrix();
+    Matrix(const Matrix &other);
+    Matrix(size_t n_row, size_t n_col);
+    Matrix(size_t n_row, size_t n_col, const T &initial_value);
+    ~Matrix();
+
+    Matrix &operator=(const Matrix &other);
+    Matrix &operator+=(const Matrix &other);
+    Matrix &operator-=(const Matrix &other);
+    Matrix &operator*=(const Matrix &other);
+
+    Matrix operator+(const Matrix &other);
+    Matrix operator-(const Matrix &other);
+    Matrix operator*(const Matrix &other);
+
+    bool operator==(const Matrix &other);
+
+    T &operator()(size_t i, size_t j);
+
+  private:
+    T *a;
+    size_t n_row, n_col;
+};
+
+#endif // __MATRIX_HPP__
